07d16c911696041b1a8345858643bbe4e508bb6b
Yuan Xie <shayx@nationalfibre.net>
2013-12-19 19:52:34 -0500

    1) ARCH-350 Making/Enabling BaseAmqpBundle. 2) Fixing Unit tests.

---
+++ ICBaseAmqpBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace IC\Bundle\Base\AmqpBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Base AMQP bundle
+ *
+ * @author Yuan Xie <shayx@nationalfibre.net>
+ */
+class ICBaseAmqpBundle extends Bundle
+{
+}
